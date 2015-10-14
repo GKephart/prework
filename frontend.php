91be8e1a75c5a34f9a83c1e4ee6675376cb6a257
George Kephart <Gkephart@cnm.edu>
2015-10-14 17:10:42 -0600

    Began writing Persona.

--- frontend.php
+++ frontend.php
@@ -1,7 +1,13 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: George
- * Date: 10/14/15
- * Time: 4:01 PM
- */
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8"/>
+		<title>data</title>
+	</head>
+	<body>
+		<h1>Data Design Project </h1>
+			<A HREF="https://www.youtube.com/watch?v=WPvGqX-TXP0">Click here for youtube</A> /link added for easy access to video
+		<h2>Persona of a person who watchs youtube videos on Java.</h2>
+			<p>A person who watches youtube videos Java coding, is a person who is learning, relearning, computer coding. This person is likely very computer savvy and already has some training in computer coding.   </p>
+	</body>
+</html>

20c97cfcb0622b06818d0434dfc3f8367fd9f3fd
George Kephart <Gkephart@cnm.edu>
2015-10-14 23:34:05 -0600

    Persona rough draft 1.1. created first user case put in ordered pair

--- frontend.php
+++ frontend.php
@@ -5,9 +5,22 @@
 		<title>data</title>
 	</head>
 	<body>
+
 		<h1>Data Design Project </h1>
-			<A HREF="https://www.youtube.com/watch?v=WPvGqX-TXP0">Click here for youtube</A> /link added for easy access to video
-		<h2>Persona of a person who watchs youtube videos on Java.</h2>
-			<p>A person who watches youtube videos Java coding, is a person who is learning, relearning, computer coding. This person is likely very computer savvy and already has some training in computer coding.   </p>
+			<A HREF="https://www.youtube.com/watch?v=WPvGqX-TXP0">Click here for youtube</A>
+		<h2>Persona and Demographics.</h2>
+
+			<p>The major demographic of users who watch youtube tutorials on Java coding, are either learning how to code in java, or are using the video as reference material, while they are coding. They are most likely computer savvy and have basic understanding of computer coding. This demographic of user would also expect that the type of youtube video they are looking for could be found using some type of search function. This demographic would also demand a rating system on youtube to ensure they are watching a high quality video, and the information is correct. Finally, the most important feature this demographic would need, is the ability to pause and jump around in the video to ensure they got the most information possible.</p>
+
+		<h2>User Case</h2>
+
+			<ol>
+			<li>Go to youtube</li>
+				<li>search For videos about Java </li>
+					<li>Find Video most relavent to what you are searching for</li>
+						<li>make sure content in video is accurate</li>
+							<li>Pause, fast-foreword, and rewind video as needed</li>
+								<li>comment or upvote if the video was useful.</li>
+			</ol>
 	</body>
 </html>

0a951688287e6c14efa6c7376970c33f8251dc21
George Kephart <Gkephart@cnm.edu>
2015-10-18 02:32:07 -0600

    Added background color to youtube, removed centering on h2 tag, fixed some typo errors. i might try and add margins other than that all structural changes have been made. i need to change the color on my svg chart, and fix content of my site to match my charts. sql databases also need to be created.

--- frontend.php
+++ frontend.php
@@ -6,7 +6,8 @@
 		<title>data</title>
 		<style>
 			h1 {text-align:center;}
-			h2{text-align: center}
+			p { text-align:justify;}
+			body {background-color:lightgray}
 		</style>
 	</head>
 
@@ -18,23 +19,23 @@
 		<h2>Persona and Demographics.</h2>
 
 
-			<p>The major demographic of users who watch youtube tutorials on Java coding, are either learning how to code in java, or are using the video as reference material, while they are coding. Micah is one such user, he is 25, has a moderate understanding of coding, and is watching the video on his laptop using Chrome on a wifi connection. Micah would expect the type of youtube video he was looking for could be found using some type of search function. Micah would also demand some type of rating system on youtube to ensure that he is watching a high quality video, and the information is correct. Finally, the most important feature Micah would need, is the ability to pause and jump around in the video to ensure he got the most information possible from the video.</p>
+			<p>The major demographic of users who watch youtube tutorials on Java coding, are either learning how to code in java, or are using the video as reference material, while they are coding. Micah is one such user, he is 25, has a moderate understanding of coding, and is watching the video on his laptop using Chrome on a wifi connection. Micah would expect the type of youtube video he was looking for could be found using some type of search function. Micah would also need some type of rating system on youtube to ensure that he is watching a high quality video, and the information is correct. Finally, the most important feature Micah would want, is the ability to pause and jump around in the video to ensure he got the most information possible from the video.</p>
 
 		<h3>User Case</h3>
-<h4>Micahs goal is to find a video on Java coding</h4>
+<h4>Micahs goal is to find a video on Java coding.</h4>
 		<ol>
-			<li>Micah goes to youtube</li>
-			<li>browser loads youtube home page</li>
-			<li>Micah searches for videos about Java </li>
-			<li>Youtube searches database for videos that are relevant to Java</li>
+			<li>Micah goes to youtube.</li>
+			<li>browser loads youtube home page.</li>
+			<li>Micah searches for videos about Java.</li>
+			<li>Youtube searches database for videos that are relevant to Java.</li>
 			<li>Micah looks for a video relavent to what he was looking for, and clicks on the video.</li>
-			<li>Youtube loads video in a new page </li>
-			<li>Micah checks comments to make sure video is accurate and presses play</li>
+			<li>Youtube loads video in a new page. </li>
+			<li>Micah checks comments to make sure video is accurate and presses play.</li>
 			<li>Youtube begins streaming video and checks Micahs connection to decide what quality of video to stream. </li>
 			<li>Micah pauses or fast forewords as needed.</li>
-			<li>Youtubes video player registers Micahs actions and manipulates the video as needed</li>
+			<li>Youtubes video player registers Micahs actions and manipulates the video as needed.</li>
 			<li>Micah finishes video and comments, upvotes the video because it was useful.</li>
-			<li>Youtube adds comment to the comment thread and increases the number the upvotes on the video, which in theory would make the video easier to find for other users</li>
+			<li>Youtube adds comment to the comment thread and increases the number the upvotes on the video, which in theory would make the video easier to find for other users.</li>
 		</ol>
 
 		<h3>Control Schema</h3>
@@ -45,7 +46,7 @@
 					<li>Account id</li>
 					<li>Profile name</li>
 					<li>email</li>
-					<li>Videos this person has uploaded</li>
+					<li>Videos this person has uploaded.</li>
 					<li>Profile Information</li>
 				</ul>
 			<h5>Relationship</h5>
